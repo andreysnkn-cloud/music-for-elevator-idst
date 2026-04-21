112dc7304c104a07b3e91d43c03e3707c3e0fc16
xauvlad <hayvlad04@mail.ru>
2026-04-21 22:50:53 +0300

    edited vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['music-for-elevator-xauvlad.amvera.io', '.amvera.io']
+  }
 });
